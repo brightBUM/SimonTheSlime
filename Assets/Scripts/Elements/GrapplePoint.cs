f006d6ff3c374f8aff5d20ae4ddcb1b3e2e3fbc4
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2024-08-12 19:46:31 +0530

    drone smoke

--- Assets/Scripts/Elements/GrapplePoint.cs
+++ Assets/Scripts/Elements/GrapplePoint.cs
@@ -1,32 +1,43 @@
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Diagnostics;
+using UnityEngine.Splines;
+using static UnityEngine.Rendering.DebugUI;
 
 public class GrapplePoint : MonoBehaviour
 {
     [SerializeField] GameObject grappleUIKey;
     [SerializeField] Transform rangeVisual;
+    [SerializeField] float dropTimerMax = 5f;
     [SerializeField] float rangeShrinkScale = 0.15f;
     [SerializeField] float rangeNormalScale = 2.2f;
     [SerializeField] GameObject playerDummy;
+    [SerializeField] ParticleSystem smokeFX;
+    float timer = 0;
+    ParticleSystem.EmissionModule emissionModule;
+    private TweenerCore<float, float, FloatOptions> Tween;
+
     // Start is called before the first frame update
     void Start()
     {
+        emissionModule = smokeFX.emission;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<PlayerController>(out PlayerController playerController))
         {
-            grappleUIKey.SetActive(true);
+            grappleUIKey.SetActive(false);
             playerController.SetGrapplePoint(this.transform.position);
             playerController.GrappleRangeShrink += GrapplePointShrink;
             playerController.GrappleRelaunch += GrappleRelaunched;
@@ -36,7 +47,7 @@ public class GrapplePoint : MonoBehaviour
     {
         if (collision.TryGetComponent<PlayerController>(out PlayerController playerController))
         {
-            grappleUIKey.SetActive(false);
+            grappleUIKey.SetActive(true);
             playerController.FreeGrapplePoint();
             playerController.GrappleRangeShrink -= GrapplePointShrink;
             playerController.GrappleRelaunch -= GrappleRelaunched;
@@ -48,11 +59,32 @@ public class GrapplePoint : MonoBehaviour
     {
         rangeVisual.DOScale(rangeShrinkScale, 0.2f).SetEase(Ease.OutBounce);
         playerDummy.SetActive(true);
+
+        DOVirtual.DelayedCall(2f, () =>
+        {
+            if (!playerDummy.activeSelf)
+                return;
+            float emissionrate = 5f;
+            emissionModule.rateOverTime = emissionrate;
+            Tween =  DOTween.To(() => emissionrate, x => emissionrate = x, 20f, 3f).OnUpdate(() =>
+            {
+                if (!playerDummy.activeSelf)
+                {
+                    DOTween.Kill(Tween);
+                    emissionrate = 0f;
+                }
+                emissionModule.rateOverTime = emissionrate;
+                
+            });
+        });
+        
+        
     }
     private void GrappleRelaunched()
     {
         playerDummy.SetActive(false);
-
+        emissionModule = smokeFX.emission;
+        emissionModule.rateOverTime = 0f;
     }
 
 }
