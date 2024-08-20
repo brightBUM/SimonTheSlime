53dcb0f34c4a8558a966e34efc586786ea444900
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2024-08-20 16:04:00 +0530

    pound animations modified

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -373,7 +373,7 @@ public class PlayerController : MonoBehaviour
         rb.velocity = Vector2.zero;
         playerAnimation.SetRoll();
         
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.2f);
 
         rb.velocity = Vector2.down * poundForce;
         ResetGravity();
