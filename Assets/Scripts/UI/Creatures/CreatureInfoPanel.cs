c859fd42234941869b7dea61ad43e23a12a38f14
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2026-06-09 16:57:28 +0530

    creature info panel color and mask fix

--- Assets/Scripts/UI/Creatures/CreatureInfoPanel.cs
+++ Assets/Scripts/UI/Creatures/CreatureInfoPanel.cs
@@ -4,13 +4,16 @@ using UnityEngine.UI;
 
 public class CreatureInfoPanel : MonoBehaviour
 {
+    [SerializeField] Image bgImage;
     [SerializeField] Image creatureImg;
     [SerializeField] TextMeshProUGUI nameText;
     [SerializeField] TextMeshProUGUI weight_regionText;
     [SerializeField] TextMeshProUGUI descText;
     [SerializeField] TextMeshProUGUI unq_descText;
+    [SerializeField] Color[] creatureColors;
     public void SetInfoData(CreatureData creatureData)
     {
+        bgImage.color = creatureColors[(int)creatureData.creatureType];
         creatureImg.sprite = creatureData.sprite;
         nameText.text = creatureData.name;
         var joinedText = creatureData.weight + " / "+creatureData.region;
